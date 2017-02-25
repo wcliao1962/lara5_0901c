c7062e5f90decfeb74a5bc1e01e7053e75a3bce8
wen-chung liao <wcliao1962@gmail.com>
2017-02-25 23:14:25 +0800

    CRUD_練習6，一樣使用HTML的<form>，但須搭配使用method_field('PATCH')，若要防止CSRF，可用csrf_field()

--- app/Http/Controllers/AdminPostsController.php
+++ app/Http/Controllers/AdminPostsController.php
@@ -23,9 +23,17 @@ class AdminPostsController extends Controller
 
     public function edit($id)
     {
-        $data = ['id' => $id];
+        $post=Post::find($id);
+        $data = ['post' => $post];
 
         return view('admin.posts.edit', $data);
+    }
+    public function update(Request $request, $id)
+    {
+        $post=Post::find($id);
+        $post->update($request->all());
+
+        return redirect()->route('admin.posts.index');
     }
     public function store(Request $request)
     {
--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,7 @@ Route::group(['prefix' => 'admin'], function() {
     Route::get('posts'          , ['as' => 'admin.posts.index' , 'uses' => 'AdminPostsController@index']);
     Route::get('posts/create'   , ['as' => 'admin.posts.create', 'uses' => 'AdminPostsController@create']);
     Route::get('posts/{id}/edit', ['as' => 'admin.posts.edit'  , 'uses' => 'AdminPostsController@edit']);
+    Route::patch('posts/{id}',    ['as' => 'admin.posts.update', 'uses' => 'AdminPostsController@update']);
     Route::post('posts', ['as' => 'admin.posts.store'  , 'uses' => 'AdminPostsController@store']);
 });
 
