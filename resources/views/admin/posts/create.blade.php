6c3d82b0e61125d5ceee75ec043f82f76cc61f7f
wen-chung liao <wcliao1962@gmail.com>
2017-02-25 16:57:49 +0800

    CRUD_練習4_5，使用HTML的<form>，未使用Form builder

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -30,21 +30,22 @@
 
 <div class="row">
     <div class="col-lg-12">
-        <form role="form">
+        <form action="/admin/posts" method="POST" role="form">
+            {{ csrf_field() }}
 
             <div class="form-group">
                 <label>標題：</label>
-                <input class="form-control" placeholder="請輸入文章標題">
+                <input name="title" class="form-control" placeholder="請輸入文章標題">
             </div>
 
             <div class="form-group">
                 <label>內容：</label>
-                <textarea class="form-control" rows="10"></textarea>
+                <textarea name="content" class="form-control" rows="10"></textarea>
             </div>
 
             <div class="form-group">
                 <label>精選？</label>
-                <select class="form-control">
+                <select name="is_feature" class="form-control">
                     <option value="0">否</option>
                     <option value="1">是</option>
                 </select>
