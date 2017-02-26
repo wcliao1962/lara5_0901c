507fb2913daf7722addb4fd51bb5c882ed885a2b
wen-chung liao <wcliao1962@gmail.com>
2017-02-26 15:04:50 +0800

    Validation_練習3，練習4

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -17,15 +17,19 @@
     </div>
 </div>
 <!-- /.row -->
-
+@if($errors->any())
 <div class="row">
     <div class="col-lg-12">
         <div class="alert alert-danger alert-dismissable">
             <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
             <i class="fa fa-info-circle"></i>  <strong>警告！</strong> 請修正表單錯誤：
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
         </div>
     </div>
 </div>
+@endif
 <!-- /.row -->
 
 <div class="row">
